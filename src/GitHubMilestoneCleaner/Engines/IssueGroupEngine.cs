b098b42ee69eb593335e59c8be986879f846df2c
Nils Andresen <nils@nils-andresen.de>
2022-11-30 23:25:10 +0100

    (#5) add comments to issues that are removed from the Milestone.

--- src/GitHubMilestoneCleaner/Engines/IssueGroupEngine.cs
+++ src/GitHubMilestoneCleaner/Engines/IssueGroupEngine.cs
@@ -3,47 +3,46 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using Octokit;
 
-namespace GitHubMilestoneCleaner.Engines
+namespace GitHubMilestoneCleaner.Engines;
+
+public class IssueGroupEngine
 {
-    public class IssueGroupEngine
-    {
-        private  readonly Regex _versionMatcher =
-            new Regex(
-                @"(0|[1-9]\d*)(\.(0|[1-9]\d*))+(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?");
+    private  readonly Regex _versionMatcher =
+        new Regex(
+            @"(0|[1-9]\d*)(\.(0|[1-9]\d*))+(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?");
         
-        public IEnumerable<IssueGroup> GroupIssues(IEnumerable<Issue> issues)
-        {
+    public IEnumerable<IssueGroup> GroupIssues(IEnumerable<Issue> issues)
+    {
 
-            return issues
-                .Select(x => new
+        return issues
+            .Select(x => new
+            {
+                Issue = x,
+                VersionAgnosticName = _versionMatcher.Replace(x.Title, string.Empty)
+            })
+            .GroupBy(x => x.VersionAgnosticName)
+            .Select(x =>
+            {
+                var issuesInGroup = 
+                    x.ToList()
+                        .OrderByDescending(y => y.Issue.Number)
+                        .ToList();
+                var main = issuesInGroup.First();
+                var subIssues = issuesInGroup.Skip(1).Select(y => y.Issue).ToList();
+
+                return new IssueGroup
                 {
-                    Issue = x,
-                    VersionAgnosticName = _versionMatcher.Replace(x.Title, string.Empty)
-                })
-                .GroupBy(x => x.VersionAgnosticName)
-                .Select(x =>
-                {
-                    var issuesInGroup = 
-                        x.ToList()
-                            .OrderByDescending(y => y.Issue.Number)
-                            .ToList();
-                    var main = issuesInGroup.First();
-                    var subIssues = issuesInGroup.Skip(1).Select(y => y.Issue).ToList();
+                    MatchKey = main.VersionAgnosticName,
+                    MainIssue = main.Issue,
+                    SubIssues = subIssues,
+                };
+            });
+    }
 
-                    return new IssueGroup
-                    {
-                        MatchKey = main.VersionAgnosticName,
-                        MainIssue = main.Issue,
-                        SubIssues = subIssues
-                    };
-                });
-        }
-
-        public class IssueGroup
-        {
-            public string MatchKey { get; set; }
-            public Issue MainIssue { get; set; }
-            public IEnumerable<Issue> SubIssues { get; set; }
-        }
+    public class IssueGroup
+    {
+        public string MatchKey { get; set; }
+        public Issue MainIssue { get; set; }
+        public IEnumerable<Issue> SubIssues { get; set; }
     }
 }
---
+++ src/GitHubMilestoneCleaner/Extension/GitHubClientExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Octokit;
+using Polly;
+using Polly.Retry;
+using Spectre.Console;
+
+namespace GitHubMilestoneCleaner.Extension;
+
+public static class GitHubClientExtensions
+{
+    private const string CancellationTokenSourceKey = "cancellationToken"; 
+    private static readonly AsyncRetryPolicy Retry = 
+        Policy
+            .Handle<ApiException>()
+            .WaitAndRetryAsync(
+                10,
+                (_, exception, _) =>
+                {
+                    var secondsToWait = exception switch
+                    {
+                        AbuseException ex1 =>
+                            ex1.RetryAfterSeconds.HasValue
+                                ? ex1.RetryAfterSeconds.Value * 1.5
+                                : 30,
+                        RateLimitExceededException ex2 => 
+                            (ex2.Reset - DateTimeOffset.Now).TotalSeconds * 1.5,
+                        _ => 30,
+                    };
+
+                    return TimeSpan.FromSeconds(secondsToWait);
+                },
+                async (ex, _, _, ctx) =>
+                {
+                    var cancellationTokenSource = (CancellationTokenSource)ctx[CancellationTokenSourceKey];
+                    if (ex is Octokit.NotFoundException)
+                    {
+                        AnsiConsole.MarkupLine($"[red]{ex.GetType().Name}: {ex.Message}[/]");
+                        cancellationTokenSource.Cancel();
+                    }
+                });
+        
+    public static async Task<T> WithRetry<T>(
+        this GitHubClient client,
+        Func<GitHubClient, Task<T>> operation)
+    {
+        var trappedClient = client;
+        var cancellationTokenSource = new CancellationTokenSource();
+        var policyContext = new Context("RetryContext") 
+        {
+            { CancellationTokenSourceKey, cancellationTokenSource }, 
+        };
+        return await Retry.ExecuteAsync(
+            async (ctx, _) => await operation(trappedClient),
+            policyContext,
+            cancellationTokenSource.Token);
+    }
+}
